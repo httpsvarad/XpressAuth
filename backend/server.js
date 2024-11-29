6cc9616b7432cb0a925fee12b034c687c62e0e43
Varad Manegopale <varad.manegopale28@gmail.com>
2024-11-29 18:16:00 +0530

    Deploy

--- backend/server.js
+++ backend/server.js
@@ -4,26 +4,30 @@ import cors from 'cors';
 import cookieParser from 'cookie-parser';
 import { connectDB } from './db/connectDB.js';
 import authRoutes from './routes/auth.route.js';
-import path from 'path';
-import { fileURLToPath } from 'url';
+// import path from 'path';
+// import { fileURLToPath } from 'url';
 
 const app = express();
 dotenv.config();
 
 // __dirname replacement in ES modules
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename);
 
 app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
 app.use('/user/auth', authRoutes);
 
-app.use(express.static(path.resolve(__dirname, "../frontend/dist")));
+// app.use(express.static(path.resolve(__dirname, "../frontend/dist")));
 
-app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "../frontend/dist/index.html"));
-});
+// app.get("*", (req, res) => {
+//     res.sendFile(path.resolve(__dirname, "../frontend/dist/index.html"));
+// });
+
+app.get('/', (req, res)=>{
+    res.send('Server Is Running')
+})
 
 app.listen(3000, () => {
     connectDB();
